fbd2df7e74c7a537b29131b5087e3ec1771f081d
FKhushbu927 <jannateekhushbu927@gmail.com>
2025-02-01 22:18:55 +0600

    1st commit of browser-navigation added

--- src/main/java/BrowserNavigation.java
+++ src/main/java/BrowserNavigation.java
@@ -1,5 +1,19 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class BrowserNavigation {
     public static void main(String[] args) {
 
+        WebDriverManager.firefoxdriver().clearDriverCache().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.linkText("Documentation")).click();
+
+        driver.navigate().refresh();
     }
 }
